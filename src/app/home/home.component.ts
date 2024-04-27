c437355e50d11b355ea02cadde06abe73c889a90
Francis <francisromasanta12@gmail.com>
2024-04-28 06:54:45 +0800

    add async await and refactor data binding

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -13,6 +13,7 @@ export class HomeComponent implements OnInit {
   user: any;
 
   weather: any;
+  isSubmitting: boolean = false;
   city: any;
   temperature: number = 0;
   feelsLikeTemp: number = 0;
@@ -35,37 +36,43 @@ export class HomeComponent implements OnInit {
     });
   }
 
-  onSubmit() {;
-    this.getCoordinates(this.searchCity);
-    this.getWeather();
+  async onSubmit(city: string) {
+    try {
+      this.isSubmitting = true;
+      await this.getCoordinates(city);
+      await this.getWeather();
+      this.searchCity = city;
+    } catch (error) {
+      console.error('Error fetching weather');
+    }
+    this.isSubmitting = false;
   }
   
-  getCoordinates(city: string) {
-    this.weatherService.getCoordinates(this.searchCity).subscribe({
-        next: (res) => {
-                this.city = res;;
-                this.lat = this.city[0].lat;
-                this.lon = this.city[0].lon;
-                this.getWeather();
-        },
-        error: (error) => console.error('Error fetching coordinates:', error.message)
-    });
-}
-
-  getWeather() {
-    this.weatherService.getWeather(this.lat, this.lon).subscribe({
-      next: (res) => {
-        this.weather = res;
-        this.temperature = this.weather.main.temp;
-        this.feelsLikeTemp = this.weather.main.feels_like;
-        this.humidity = this.weather.main.humidity;
-        this.pressure = this.weather.main.pressure;
-        this.summary = this.weather.weather[0].main;
-
-        this.iconURL = 'https://openweathermap.org/img/wn/' + this.weather.weather[0].icon + '@2x.png';
-      },
-
-      error: (error) => console.log(error.message),
-    })
+  async getCoordinates(city: string): Promise<void> {
+    try {
+      const res = await this.weatherService.getCoordinates(city).toPromise();
+      this.city = res;
+      this.lat = this.city[0].lat;
+      this.lon = this.city[0].lon;
+    } catch (error) {
+      console.error('Error fetching coordinates');
+      throw error;
+    }
+  }
+  
+  async getWeather(): Promise<void> {
+    try {
+      const res = await this.weatherService.getWeather(this.lat, this.lon).toPromise();
+      this.weather = res;
+      this.temperature = this.weather.main.temp;
+      this.feelsLikeTemp = this.weather.main.feels_like;
+      this.humidity = this.weather.main.humidity;
+      this.pressure = this.weather.main.pressure;
+      this.summary = this.weather.weather[0].main;
+      this.iconURL = 'https://openweathermap.org/img/wn/' + this.weather.weather[0].icon + '@2x.png';
+    } catch (error) {
+      console.error('Error fetching weather');
+      throw error;
+    }
   }
 }
