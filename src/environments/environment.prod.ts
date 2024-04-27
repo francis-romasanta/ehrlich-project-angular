9cda8a9438ea1339dd07f363f30c62a59551b9cf
Francis <francisromasanta12@gmail.com>
2024-04-28 01:14:28 +0800

    prod build

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -4,7 +4,7 @@ export const environment = {
     domain: "dev-zrzez42a5dwn7qrz.us.auth0.com",
     clientId: "zCgM59GfwMVFCMSh46b3RRuWfOHWYbRG",
     authorizationParams: {
-      redirect_uri: 'http://localhost:4200'
+      redirect_uri: 'https://francis-romasanta.github.io/ehrlich-project-angular/'
     }
   },
 };
