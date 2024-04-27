141800f791fed0cb778a5d049f15c10926ca1719
Francis <francisromasanta12@gmail.com>
2024-04-28 02:05:06 +0800

    fix logout on prod env redirecting to localhost

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
+import { Component, Inject, OnInit } from '@angular/core';
 import { AuthService } from '@auth0/auth0-angular';
 
 @Component({
@@ -10,12 +11,15 @@ export class AppComponent implements OnInit {
   title = 'ehrlich-project';
   isLoggedIn: boolean;
 
-  constructor (public authService: AuthService) {
+  constructor (public authService: AuthService, @Inject(DOCUMENT) private document: Document) {
     this.isLoggedIn = false;
   }
   public ngOnInit(): void {
     this.authService.isAuthenticated$.subscribe((success: boolean) => {
       this.isLoggedIn = success;
     });
+  }
+  public logout(): void {
+    this.authService.logout({ logoutParams: { returnTo: this.document.location.origin } });
   }
 }
