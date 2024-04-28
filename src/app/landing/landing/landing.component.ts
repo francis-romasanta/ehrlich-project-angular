5a44f9541de8376ad0d5fe96446d06762c11d3dc
Francis <francisromasanta12@gmail.com>
2024-04-28 16:07:45 +0800

    add login button for smaller screens

--- src/app/landing/landing/landing.component.ts
+++ src/app/landing/landing/landing.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
-
+import { AuthService } from '@auth0/auth0-angular';
 @Component({
   selector: 'app-landing',
   templateUrl: './landing.component.html',
   styleUrl: './landing.component.scss'
 })
 export class LandingComponent {
-
+  constructor (public authService: AuthService) {}
 }
